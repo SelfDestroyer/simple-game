7e9cc83421a1e43db2d57bc76954225e9947bb3d
Vova Ivaniuk <ivaniuk@Vovas-MacBook-Pro.local>
2023-02-23 07:07:41 +0200

    configured project

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Simple Game</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
